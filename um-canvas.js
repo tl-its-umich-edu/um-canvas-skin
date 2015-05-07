70ecf999f6b1f17a7090d931d4cc7806a26eb888
Beth Kirschner <bkirschn@umich.edu>
2015-05-07 14:45:44 -0400

    TLUNIZIN-509 updated with production canvas id

--- um-canvas.js
+++ um-canvas.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
-  $('.context_external_tool_633').css('display','none');
+  $('.context_external_tool_660').css('display','none');
   if(ENV.current_user_roles.indexOf('admin') > -1) {
-  	$('.context_external_tool_633').css('display','block')
+  	$('.context_external_tool_660').css('display','block')
   }
 });
