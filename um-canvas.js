9c90496c78e3c6a2d8499dc09862f8e211219284
Gonzalo Silverio <gsilver@umich.edu>
2015-05-22 10:19:33 -0400

    TLUNIZIN-509 - change js to hide content migration link after class name change

--- um-canvas.js
+++ um-canvas.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
-  $('.context_external_tool_660').css('display','none');
+  $('.context_external_tool_739').css('display','none');
   if(ENV.current_user_roles.indexOf('admin') > -1) {
-  	$('.context_external_tool_660').css('display','block')
+  	$('.context_external_tool_739').css('display','block');
   }
 });
