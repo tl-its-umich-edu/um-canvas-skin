40cf4d2e1ee2e59febb3885814497e3487fa7c19
Gonzalo Silverio <gsilver@umich.edu>
2016-01-14 11:30:20 -0500

    TLUNIZIN-919 - respond to changed ID for Content Migrations tool in Canvas

--- um-canvas.js
+++ um-canvas.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
-  $('.context_external_tool_739').css('display','none');
+  $('.context_external_tool_2332').css('display','none');
   if(ENV.current_user_roles.indexOf('admin') > -1) {
-  	$('.context_external_tool_739').css('display','block');
+  	$('.context_external_tool_2332').css('display','block');
   }
 });
