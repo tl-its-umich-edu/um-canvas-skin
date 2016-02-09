23cc924f5093b9d52a717fc9bfd92a6d04325345
Gonzalo Silverio <gsilver@umich.edu>
2016-02-09 11:57:27 -0500

    modify script to hide content migrations

--- um-canvas.js
+++ um-canvas.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
-  $('.context_external_tool_2332').css('display','none');
-  if(ENV.current_user_roles.indexOf('admin') > -1) {
-  	$('.context_external_tool_2332').css('display','block');
+  if(ENV.current_user_roles.indexOf("admin") == -1) {
+    $("#section-tabs li.section:contains('Content Migrations')").remove();
   }
 });
